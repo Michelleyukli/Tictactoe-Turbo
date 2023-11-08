a73a7aac2941e38c0162e47230015e41f61d5e3d
Yulin Li <148395276+Michelleyukli@users.noreply.github.com>
2023-11-08 05:05:49 +0000

    file changed

--- cli.py
+++ cli.py
@@ -2,82 +2,34 @@ from logic import check_winner, get_empty_board, print_board, get_player_input, 
 import logging
 # add the log file record the games 
 logging.basicConfig(
-    filename='logs/msg.log',
+    filename='log/msg.log',
     level=logging.INFO
 )
-    
+         
 
-def get_empty_board():
-    """
-    row_1 = ['O', 'O', 'O']
-    row_2 = ['O', 'O', 'O']
-    row_3 = ['O', 'O', 'O']
-
-    board = [row_1, row_2, row_3]
-    """
-    return [
-        [None, None, None],
-        [None, None, None],
-        [None, None, None],
-    ]
-
-
-def print_board(board):
-    for row in board:
-        print(row)  # print will print the variable in a new line
-
-
-def get_player_input(player_input):
-    """
-    input:
-        row,col
-    return:
-        row: int -> the index of row
-        col: int -> the index of column
-    """
-    row_col_list = player_input.split(",")  # ["1", "1"]
-    row, col = [int(x) for x in row_col_list]  # [1,1]
-    return row, col
-
-
-def switch_player(current_player):
-    if current_player == "X":
-        return "O"
-    return "X"
-
-
-if __name__ == "__main__":
+if __name__ == '__main__': #program excute directly
     current_player = "X"
-    board = get_empty_board()  # get a empty board
-    winner = None
-
-    while winner is None:
-        print_board(board)  # print the board
-        player_input = input(f"player {current_player} > ")
+    board = get_empty_board()
+    winner = None 
+    
+    while winner is None: 
+        print_board(board)
         try:
-            row, col = get_player_input(player_input)  # ask user input
+           row, col = get_player_input(current_player)
+        
         except ValueError:
-            print("Invalid input, try again\n")
+            print("Invaild, try again")
+            continue
+        except IndexError:
+            print("Invaild, try again")
             continue
 
-        if row >= len(board[0]) or col >= len(board):
-            print(f"Out of bounds, try again\n")
-            continue
+        print(row)
+        print(col)
 
-        if board[row][col]:
-            print(
-                f"{row},{col} already has mark {board[row][col]}, please choose another place\n"
-            )
-            continue
-
-        # mark the board
         board[row][col] = current_player
-
-        # check for winner
-        winner = check_winner(board)  # "O", "X" -> break out of the loop
-
+        winner = check_winner(board)
         current_player = switch_player(current_player)
-        print("\n")
-        # current_player = "X" if current_player == "O" else "O"
-    print_board(board)
-    print(f"Winner is {winner}")
+print_board(board)
+print(f"winner is {winner}")
+logging.info(f'Player {current_player} made a move')
